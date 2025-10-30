8fff8f41f8f547a4d6fc45a64027eb008824e583
Ares <ikaperez25@lhusurbil.eus>
2025-10-30 10:12:54 +0100

    testa

--- Erronka PCPM/src/app/App.java
+++ Erronka PCPM/src/app/App.java
@@ -1,5 +1,8 @@
 package app;
 public class App {
+    static String agurra(){
+        return "Kaixo";
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
