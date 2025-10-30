987f731c863eb789f226f987784df080ec55b8fc
Ares <ikaperez25@lhusurbil.eus>
2025-10-30 12:34:34 +0100

    testa berriro

--- Erronka PCPM/src/APLIKAZIOA/testa.java
+++ Erronka PCPM/src/APLIKAZIOA/testa.java
@@ -1,11 +1,11 @@
 package APLIKAZIOA;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+
 public class testa{
     @Test
-
     public void testagurra(){
         String agurra = App.agurra();
-assertEquals("Kaixo", agurra);
+        assertEquals("Kaixo", agurra);
     }
 }
