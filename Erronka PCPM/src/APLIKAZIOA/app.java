5516bf8c75cc3a02c70f74dd955b944e600097be
Ares <ikaperez25@lhusurbil.eus>
2025-11-10 11:47:31 +0100

    aplikazioa konponduta

--- Erronka PCPM/src/APLIKAZIOA/app.java
+++ Erronka PCPM/src/APLIKAZIOA/app.java
@@ -7,7 +7,6 @@ public class app {
     private static HashMap<String, ArrayList<Integer>> egutegia = new HashMap<>();
     
     static {
-      
         ArrayList<Integer> astelehena = new ArrayList<>();
         astelehena.add(4);
         astelehena.add(2); 
@@ -33,17 +32,6 @@ public class app {
         ostirala.add(3); 
         ostirala.add(4); 
         egutegia.put("Ostirala", ostirala);
-        
-        ArrayList<Integer> larunbata = new ArrayList<>();
-        larunbata.add(1);
-        larunbata.add(2); 
-        larunbata.add(4); 
-        egutegia.put("Larunbata", larunbata);
-        
-        ArrayList<Integer> igandea = new ArrayList<>();
-        igandea.add(0);
-        igandea.add(4); 
-        egutegia.put("Igandea", igandea);
     }
     
 
@@ -52,9 +40,9 @@ public class app {
     }
     
     public static void main(String[] args) {
-  
         hasierakoMenua menu = new hasierakoMenua();
         zinea cine = new zinea();
+        aukeratuAsteburua asteburuaAukeraketa = new aukeratuAsteburua(cine);
         Scanner sc = new Scanner(System.in);
         
         while (menu.dagoProgramaMartxan()) {
@@ -76,6 +64,8 @@ public class app {
                 erakutsiKokapena();
             } else if (aukeratutakoa.equals("Ordutegia")) {
                 erakutsiOrdutegia();
+            } else if (aukeratutakoa.equals("Aukeratu data (asteburuak)")) {
+                prozesatuAsteburuaAukeraketa(asteburuaAukeraketa, sc);
             } else if (aukeratutakoa.equals("Itxi")) {
                 menu.itxiPrograma();
                 System.out.println("Agur!");
@@ -84,7 +74,82 @@ public class app {
         sc.close();
     }
     
-  
+    private static void prozesatuAsteburuaAukeraketa(aukeratuAsteburua asteburuaAukeraketa, Scanner sc) {
+        asteburuaAukeraketa.erakutsiHilabeteak();
+        int hilabeteAukera = sc.nextInt();
+        
+        if (hilabeteAukera < 1 || hilabeteAukera > asteburuaAukeraketa.getHilabeteKopurua()) {
+            System.out.println("Hilabete okerra aukeratu duzu");
+            return;
+        }
+        
+        String hilabetea = asteburuaAukeraketa.getHilabetea(hilabeteAukera - 1);
+        System.out.println("Aukeratu duzun hilabetea: " + hilabetea);
+        
+        asteburuaAukeraketa.erakutsiAsteburuZenbakiak();
+        int asteburuZenbakiAukera = sc.nextInt();
+        
+        if (asteburuZenbakiAukera < 1 || asteburuZenbakiAukera > asteburuaAukeraketa.getAsteburuZenbakiKopurua()) {
+            System.out.println("Asteburu zenbakia okerra aukeratu duzu");
+            return;
+        }
+        
+        String asteburuZenbakia = asteburuaAukeraketa.getAsteburuZenbakia(asteburuZenbakiAukera - 1);
+        
+        asteburuaAukeraketa.erakutsiAsteBuruak();
+        int asteburuAukera = sc.nextInt();
+        
+        if (asteburuAukera < 1 || asteburuAukera > asteburuaAukeraketa.getAsteburuKopurua()) {
+            System.out.println("Asteburuko eguna okerra aukeratu duzu");
+            return;
+        }
+        
+        String asteburua = asteburuaAukeraketa.getAsteburua(asteburuAukera - 1);
+        
+        System.out.println("\nAukeratu duzun data: " + asteburua + ", " + asteburuZenbakia.toLowerCase() + " " + hilabetea + "n");
+        
+        asteburuaAukeraketa.erakutsiEgunekoFilmak();
+        
+        System.out.println("\nErosi sarrerak asteburu honetarako? (Bai/Ez)");
+        sc.nextLine(); 
+        String erantzuna = sc.nextLine();
+        
+        if (erantzuna.equalsIgnoreCase("Bai")) {
+            prozesatuErosketaAsteburua(asteburuaAukeraketa.getAzkenPelikulak(), sc);
+        } else {
+            System.out.println("Itzuli menura...");
+        }
+    }
+    
+    private static void prozesatuErosketaAsteburua(ArrayList<pelikula> pelikulak, Scanner sc) {
+        System.out.println("\nAukeratu filma:");
+        for (int i = 0; i < pelikulak.size(); i++) {
+            pelikula p = pelikulak.get(i);
+            System.out.println((i + 1) + ". " + p.getKolorea() + p.getTitulua() + koloreak.RESET + 
+                             " - Stock: " + p.getStock());
+        }
+        
+        int pelikulaAukera = sc.nextInt() - 1;
+        
+        if (pelikulaAukera < 0 || pelikulaAukera >= pelikulak.size()) {
+            System.out.println("Filma okerra aukeratu duzu.");
+            return;
+        }
+        
+        pelikula aukeratutakoPelikula = pelikulak.get(pelikulaAukera);
+        
+        System.out.println("Zenbat sarrera nahi dituzu?");
+        int sarrerak = sc.nextInt();
+        
+        if (sarrerak >= 1 && sarrerak <= aukeratutakoPelikula.getStock()) {
+            aukeratutakoPelikula.kenduStock(sarrerak);
+            System.out.println("Erosketa ondo egin da!");
+            System.out.println("Geratzen den stocka: " + aukeratutakoPelikula.getStock());
+        } else {
+            System.out.println("Sartutako sarrera kantitatea ez dugu disponible");
+        }
+    }
+    
     private static void prozesatuAstekoEgunak(zinea cine, Scanner sc) {
         ArrayList<String> astekoegunak = new ArrayList<>();
         astekoegunak.add("Astelehena");
@@ -92,8 +157,6 @@ public class app {
         astekoegunak.add("Asteazkena");
         astekoegunak.add("Osteguna");
         astekoegunak.add("Ostirala");
-        astekoegunak.add("Larunbata");
-        astekoegunak.add("Igandea");
         
         System.out.println("Aukeratu asteko eguna");
         for (int j = 0; j < astekoegunak.size(); j++) {
